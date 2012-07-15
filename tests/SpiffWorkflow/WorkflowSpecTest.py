affc61bc75d777f5aceef75ff1f4b391b109d5e8
Ziad Sawalha <github@highbridgellc.com>
2012-07-15 00:03:16 -0500

    Add Cyclic Wait Test
    - when tasks are waiting on each other, the
      workflow never completes. We should be able to
      detect simple cases of this situation before
      running the workflow (at least with Join and
      Merge task types). This code achieves that.

---
+++ tests/SpiffWorkflow/WorkflowSpecTest.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import unittest
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+
+from SpiffWorkflow.specs import Join, WorkflowSpec
+
+
+class WorkflowSpecTransformTest(unittest.TestCase):
+    def setUp(self):
+        self.wf_spec = WorkflowSpec()
+
+    def test_cyclic_wait(self):
+        """
+        Tests that we can detect when two wait taks are witing on each other.
+        """
+        task1 = Join(self.wf_spec, 'First')
+        self.wf_spec.start.connect(task1)
+        task2 = Join(self.wf_spec, 'Second')
+        task1.connect(task2)
+
+        task2.follow(task1)
+        task1.follow(task2)
+
+        results = self.wf_spec.validate()
+        self.assertIn("Found loop with 'Second': Second->First then 'Second' "
+                "again", results)
+        self.assertIn("Found loop with 'First': First->Second then 'First' "
+                "again", results)
+
+
+def suite():
+    return unittest.TestLoader().loadTestsFromTestCase(
+            WorkflowSpecTransformTest)
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
